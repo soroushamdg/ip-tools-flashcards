32b1ee9d62e5fb64740e4a87e0f2cee3413a174c
Soroush M Pour <soroushmpour@gmail.com>
2020-05-15 22:52:20 +0430

    + crawler  + added TEST_CRAWLER.py and test cases for request.py and extractor.py

---
+++ crawler/TEST_CRAWLER.py
@@ -0,0 +1,27 @@
+import unittest
+from crawler import extractor, requester, saver
+
+class TestCode(unittest.TestCase):
+    def test_request(self):
+        requester_1 = requester.requester('http://apple.com')
+        self.assertEqual(requester_1.extract_html()['error'],False)
+
+        requester_2 = requester.requester('https://www.w3schools.com/python/python_datetime.asp')
+        self.assertEqual(requester_2.extract_html()['error'], False)
+
+        requester_3 = requester.requester('http://google.com')
+        self.assertEqual(requester_3.extract_html()['error'], False)
+
+    def test_extracter(self):
+
+        test_1 = requester.requester('https://www.w3schools.com/python/python_ref_list.asp').extract_html()
+        extracter_1 = extractor.extracter_from_w3(test_1['content'])
+        self.assertNotEqual(extracter_1.extract_data(),False)
+
+        test_2 = requester.requester('https://www.w3schools.com/python/python_ref_dictionary.asp').extract_html()
+        extracter_2 = extractor.extracter_from_w3(test_2['content'])
+        self.assertNotEqual(extracter_2.extract_data(), False)
+
+        test_3 = requester.requester('https://www.w3schools.com/python/python_ref_functions.asp').extract_html()
+        extracter_3 = extractor.extracter_from_w3(test_3['content'])
+        self.assertNotEqual(extracter_3.extract_data(), False)
